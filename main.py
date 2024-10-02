14dba41aba000a2a2098a246147fa84b6fb480c7
Chaimae BELEMOUALEM <SE12972@inetpsa.com>
2024-10-02 02:04:51 +0200

    updating the port of mongodb

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 app = FastAPI()
 
 #Connecting to mongodb bookstore database 
-client = AsyncIOMotorClient('mongodb://mongo:27017')
+client = AsyncIOMotorClient('mongodb://localhost:27018')
 db = client['bookstore']
 collection = db['books']
 
@@ -27,13 +27,13 @@ def book_helper(book) -> dict:
         "summary": book["summary"]
     }
 
-# GET /books : Récupérer tous les livres
+# GET /books : get all books
 @app.get("/books", response_model=List[Book])
 async def get_books():
     books = await collection.find().to_list(100)
     return [book_helper(book) for book in books]
 
-# GET /books/{book_id} : Récupérer un livre par ID
+# GET /books/{book_id} : get book by id 
 @app.get("/books/{book_id}", response_model=Book)
 async def get_book(book_id: str):
     book = await collection.find_one({"_id": ObjectId(book_id)})
@@ -41,22 +41,18 @@ async def get_book(book_id: str):
         raise HTTPException(status_code=404, detail="Livre non trouvé")
     return book_helper(book)
 
+# POST create a new book and return the id of the book is being returned 
 @app.post("/books", response_model=dict)
 async def create_book(book: Book):
     try:
-        print(book)
         book_data = {
             "title": book.title,
             "author": book.author,
             "summary": book.summary,
         }
-        print(book_data)
-
-        # Attempt to insert the book data into the collection
+        
         result = await collection.insert_one(book_data)
-
-        #Fetch the newly created book from the database
-        new_book = await collection.find_one({"_id": result.inserted_id})
+        new_book = await collection.find_one({"_id": ObjectId(result.inserted_id)})
         
         if new_book is None:
             raise HTTPException(status_code=500, detail="Failed to create book")
@@ -67,10 +63,10 @@ async def create_book(book: Book):
         logging.error(f"Error creating book: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# PUT /books/{book_id} : Mettre à jour un livre existant
+# PUT /books/{book_id} : update an already existing book 
 @app.put("/books/{book_id}", response_model=dict)
 async def update_book(book_id: str, book: Book):
-    # Create a dictionary manually from the Pydantic model
+
     book_data = {
         "title": book.title,
         "author": book.author,
@@ -78,28 +74,26 @@ async def update_book(book_id: str, book: Book):
     }
     
     try:
-        # Attempt to update the book in the collection
+
         updated_book = await collection.find_one_and_update(
             {"_id": ObjectId(book_id)},
             {"$set": book_data},
             return_document=True
         )
         
-        # Check if the book was found and updated
         if updated_book is None:
             raise HTTPException(status_code=404, detail="Book not found")
         
         return book_helper(updated_book)
 
     except Exception as e:
-        # Log the error and raise a server error
         logging.error(f"Error updating book: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# DELETE /books/{book_id} : Supprimer un livre
+# DELETE /books/{book_id} : delete a book by ID 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: str):
     delete_result = await collection.delete_one({"_id": ObjectId(book_id)})
     if delete_result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Livre non trouvé")
-    return {"message": "Livre supprimé"}
+        raise HTTPException(status_code=404, detail="book not found")
+    return {"message": "Book Has beeing deleted! "}
